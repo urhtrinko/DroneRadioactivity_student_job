fe2ed80ab918a39fcc09df80bab9967f78473ff5
urh_trinko <urh.trinko@gmail.com>
2023-01-03 15:01:20 +0100

    Update practical_measurement.py

--- practical_measurement.py
+++ practical_measurement.py
@@ -18,7 +18,7 @@ noise = [5, 5] # list that contains the standard deviation of the x and y coordi
 
 
 # ZIG_ZAG PARAMETERS
-grid = 4 # Size of the grid in which the area of flyover is divided into smaller "tiles" where the detector stops and measures the number of 
+grid = 3 # Size of the grid in which the area of flyover is divided into smaller "tiles" where the detector stops and measures the number of 
          # radioactive decays. Grid is the number of these areas in x direction and y direction. It must be an INTEGER!
 
 # We have to manually fill this dictionary
@@ -29,41 +29,49 @@ detector = {"h": h, "dt": dt, "x_max": x_max, "y_max": y_max, "grid": grid}#, "d
 
 ##CLASS
 
+class App( npyscreen.NPSAppManaged ):
+    def onStart( self ):
+        self.addForm( 'PARAMETERS', FormObject0, name = "Parameters input")
+        self.addForm( 'DOSES', FormObject, name = "Doses")#, lines=60), columns = 80)
+
+class FormObject0( npyscreen.ActionForm ): # Action -> Cancel/OK
+    def create( self ):
+        pass
+        # all the parameters #
+    
+    def afterEditing( self ):
+        # self.parentApp.setNextForm( None )
+        pass
+
+    def on_ok(self):
+        npyscreen.notify_confirm("Form has been saved.", "Saved" , editw=1)
+        self.parentApp.switchForm("DOSES")
+        # pass
+        # OK button pressed.
+
+    def on_cancel(self):
+        exiting = npyscreen.notify_yes_no("Are you sure want to Cancel?", "Cancel BUTTON", editw=1)
+        if ( exiting ):
+            npyscreen.notify_confirm("OK. Form has NOT been saved.", "Good bye!")
+            self.parentApp.setNextForm(None)
+        else:
+            npyscreen.notify_confirm("You may continue working.", "Okay")
+        # pass
+        # Cancel button pressed.
+
 class FormObject( npyscreen.ActionForm ): # Action -> Cancel/OK
     def create( self ):
-        A_b = radiation['A_b']
-        h = detector['h']; dt = detector['dt']; x_max = detector['x_max']; y_max = detector['y_max']; N_grid = detector['grid']
+        
+        HD_str = "Enter HD for" + " (" + 'str(round(x, 2))' + ", " + 'str(round(y, 2))' + "):"
+        begin_HD = len(HD_str)
+        self.HD = self.add( npyscreen.TitleText, name = HD_str, begin_entry_at = begin_HD+1, use_two_lines = False)
 
-        square_x = (2*x_max)/N_grid; square_y = (2*y_max)/N_grid
+        dHD_str = "Enter dHD for" + " (" + 'str(round(x, 2))' + ", " + 'str(round(y, 2))' + "):"
+        begin_dHD = len(dHD_str)
+        self.dHD = self.add( npyscreen.TitleText, name = dHD_str, relx=3, begin_entry_at = begin_dHD+1, use_two_lines = False)
 
-        grid_x, grid_y = np.zeros((N_grid, N_grid)), np.zeros((N_grid, N_grid))
-        grid_x_noise, grid_y_noise = np.zeros((N_grid, N_grid)), np.zeros((N_grid, N_grid))
-        xs = np.linspace(-x_max + square_x/2, x_max - square_x/2, int(N_grid))
-        HDs = np.zeros((int(N_grid), int(N_grid))); dHDs = np.zeros((int(N_grid), int(N_grid)))
-
-        n, m = N_grid - 1, 0
-        y = -y_max + square_y/2
-        i = 1
-        for x in xs:
-            while abs(y) <= y_max:
-
-                HD_str = "Enter HD for" + " (" + str(round(x, 2)) + ", " + str(round(y, 2)) + "):"
-                begin_HD = len(HD_str)
-                self.HD = self.add( npyscreen.TitleText, name = HD_str, begin_entry_at = begin_HD+1, use_two_lines = False)
-                
-                dHD_str = "Enter dHD for" + " (" + str(round(x, 2)) + ", " + str(round(y, 2)) + "):"
-                begin_dHD = len(dHD_str)
-                self.dHD = self.add( npyscreen.TitleText, name = dHD_str, relx=3, begin_entry_at = begin_dHD+1, use_two_lines = False)
-
-                self.nextrely += 1
-                grid_x[n, m] = x; grid_y[n, m] = y
-                y += (square_y)*i
-                n -= 1*i
-            n += 1*i; i = i * (-1); y += (square_y)*i; m += 1
-
-
-    def __str__(self):
-        return self
+        
+        # return {"m_dose": HDs, "dm_dose": dHDs, "source": [], "grid_x": grid_x, "grid_y": grid_y, "grid_x_noise": grid_x_noise, "grid_y_noise": grid_y_noise, "hotspot": maxI_range, "square_x": square_x, "square_y": square_y, "x_max": x_max, "y_max": y_max}
 
     def afterEditing( self ):
         # self.parentApp.setNextForm( None )
@@ -82,14 +90,8 @@ class FormObject( npyscreen.ActionForm ): # Action -> Cancel/OK
             self.parentApp.setNextForm(None)
         else:
             npyscreen.notify_confirm("You may continue working.", "Okay")
-
-
         # pass
         # Cancel button pressed.
-
-class App( npyscreen.NPSAppManaged ):
-    def onStart( self ):
-        self.addForm( 'MAIN', FormObject, name = "npyscreen Form", lines=60)#, columns = 80)
 
 if ( __name__ == "__main__" ):
     app = App().run()
