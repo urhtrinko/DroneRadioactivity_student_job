5dfcf91db1c447f5f3e19d949e7837ecd2d82951
urh_trinko <urh.trinko@gmail.com>
2023-01-20 14:16:20 +0100

    Update detector.py

--- working_apps/detector_simulation/detector.py
+++ working_apps/detector_simulation/detector.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import (QApplication, QDialog, QMainWindow, QMessageBox)
+from PyQt5.QtCore import QSettings
 
 from MainWindow import Ui_MainWindow
 from describtion import Ui_Dialog
@@ -12,10 +13,23 @@ class Window(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
         self.connectingSlots()
-        
+        self.getSettingsValues()
+
+        # Set class atributes
+        self.A_b = self.settingParameters.value("Ab"); self.A_max = self.settingParameters.value("Amax"); self.A_min = self.settingParameters.value("Amin")
+        self.F = self.settingParameters.value("dose_factor")
+        self.h = self.settingParameters.value("height"); self.dt = self.settingParameters.value("dt"); self.K = self.settingParameters.value("detector_constant")
+        self.x_max = self.settingParameters.value("xmax"); self.y_max = self.settingParameters.value("ymax")
+
+        # Set text value
+        self.lineEdit_Ab.setText(self.A_b); self.lineEdit_Amax.setText(self.A_max); self.lineEdit_Amin.setText(self.A_min)
+        self.lineEdit_F.setText(self.F)
+        self.lineEdit_h.setText(self.h); self.lineEdit_dt.setText(self.dt); self.lineEdit_K.setText(self.K)
+        self.lineEdit_xmax.setText(self.x_max); self.lineEdit_ymax.setText(self.y_max)
+
+        # Additional atributes
         self.radiation = {}
         self.detector = {}
-        self.source = []
 
     def connectingSlots(self):
         self.btnSaveGenerate.clicked.connect(self.setParameters)
@@ -23,18 +37,21 @@ class Window(QMainWindow, Ui_MainWindow):
         self.btnMeasure.clicked.connect(self.locationOfmeasuremnt)
         self.btnDescribtion.clicked.connect(self.description)
 
+    def getSettingsValues(self):
+        self.settingParameters = QSettings("My App", "Parameters")
+
     def description(self):
         dialog = Dialog(self)
         dialog.exec()
 
     def setParameters(self):
-        A_b = self.lineEdit_Ab.text(); A_max = self.lineEdit_Amax.text(); A_min = self.lineEdit_Amin.text()
-        F = self.lineEdit_F.text()
-        h = self.lineEdit_h.text(); dt = self.lineEdit_dt.text(); K = self.lineEdit_K.text()
-        x_max = self.lineEdit_xmax.text(); y_max = self.lineEdit_ymax.text()
+        # A_b = self.lineEdit_Ab.text(); A_max = self.lineEdit_Amax.text(); A_min = self.lineEdit_Amin.text()
+        # F = self.lineEdit_F.text()
+        # h = self.lineEdit_h.text(); dt = self.lineEdit_dt.text(); K = self.lineEdit_K.text()
+        # x_max = self.lineEdit_xmax.text(); y_max = self.lineEdit_ymax.text()
 
-        self.radiation = {"A_min": float(A_min), "A_max": float(A_max), "A_b": float(A_b), "dose_factor": float(F)}
-        self.detector = {"h": float(h), "dt": float(dt), "x_max": float(x_max), "y_max": float(y_max), "detector_constant": float(K)}
+        self.radiation = {"A_min": float(self.A_min), "A_max": float(self.A_max), "A_b": float(self.A_b), "dose_factor": float(self.F)}
+        self.detector = {"h": float(self.h), "dt": float(self.dt), "x_max": float(self.x_max), "y_max": float(self.y_max), "detector_constant": float(self.K)}
 
     def generateSource(self):
         self.source = randSource(self.radiation, self.detector)
@@ -52,6 +69,17 @@ class Window(QMainWindow, Ui_MainWindow):
         x = self.lineEdit_x.text(); y = self.lineEdit_y.text()
         HD, dHD = fieldMeasurement(self.radiation, self.detector, self.source, self.minusInStr(x), self.minusInStr(y), [])
         self.lineEdit_HD.setText(str(round(HD, 2)) + " +/- " + str(round(dHD, 2)))
+
+    def closeEvent(self, event):
+        self.settingParameters.setValue("Ab", self.lineEdit_Ab.text())
+        self.settingParameters.setValue("Amax", self.lineEdit_Amax.text())
+        self.settingParameters.setValue("Amin", self.lineEdit_Amin.text())
+        self.settingParameters.setValue("dose_factor", self.lineEdit_F.text())
+        self.settingParameters.setValue("height", self.lineEdit_h.text())
+        self.settingParameters.setValue("dt", self.lineEdit_dt.text())
+        self.settingParameters.setValue("detector_constant", self.lineEdit_K.text())
+        self.settingParameters.setValue("xmax", self.lineEdit_xmax.text())
+        self.settingParameters.setValue("ymax", self.lineEdit_ymax.text())
 
 class Dialog(QDialog, Ui_Dialog):
     def __init__(self, parent= None):
