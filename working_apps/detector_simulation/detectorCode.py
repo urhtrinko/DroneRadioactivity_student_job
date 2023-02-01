36cc2f2d38a125fa060ef8235cab7ab3750cab3f
urh_trinko <urh.trinko@gmail.com>
2023-02-01 12:14:22 +0100

    Update detectorCode.py

--- working_apps/detector_simulation/detectorCode.py
+++ working_apps/detector_simulation/detectorCode.py
@@ -4,12 +4,10 @@ import random
 def activity(source, x, y, h, ru=0, rv=0):
     u, v, A0 = source[0], source[1], source[2] # u, v are the coordinates of the source and A0 is its activity
     return (A0*(ru**2 + rv**2 + h**2)) / ((x - (u - ru))**2 + (y - (v - rv))**2 + h**2)
-
 def randSource(radiation, detector):
     A_min = radiation['A_min']; A_max = radiation['A_max']
     x_max = detector['x_max']; y_max = detector['y_max']
     return [random.uniform(-x_max, x_max), random.uniform(-y_max, y_max), random.uniform(A_min, A_max)]
-
 def fieldMeasurement(radiation, detector, source, x, y, noise = []):
     A_b = radiation['A_b']; F = radiation['dose_factor']
     h = detector['h']; dt = detector['dt']; K = detector['detector_constant']
