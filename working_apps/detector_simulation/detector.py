1f1a283bc071d1bef3784a6ecc4e9a2a3d3f15b0
urh_trinko <urh.trinko@gmail.com>
2023-02-09 15:45:15 +0100

    Update detector.py

--- working_apps/detector_simulation/detector.py
+++ working_apps/detector_simulation/detector.py
@@ -12,6 +12,10 @@ class Window(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
+        #shorcuts for buttons
+        self.btnMeasure.setShortcut("Return")
+
         self.connectingSlots()
         self.getSettingsValues()
 
