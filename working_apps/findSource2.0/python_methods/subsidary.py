68f2f6c1b78d56e028250e7ad92654117834b500
urh_trinko <urh.trinko@gmail.com>
2023-02-10 14:26:28 +0100

    Update subsidary.py

--- working_apps/findSource2.0/python_methods/subsidary.py
+++ working_apps/findSource2.0/python_methods/subsidary.py
@@ -61,19 +61,26 @@ def count0InArray(array):
 # arr = np.array([[1, 0], [0, 1]])
 # print(count0InArray(arr))
 
+# overdwfined system of linear equations sovling function
+def parsEst2xN(HDs, grid_x, grid_y, h, u_est, v_est):
+    N = len(grid_x.flatten())
+    r = (grid_x.flatten() - np.ones((N))*u_est)**2 + (grid_y.flatten() - np.ones((N))*v_est)**2 + (np.ones((N))*h)**2
+    a = np.rot90(np.array([1/r, np.ones(N)])); b = HDs.flatten()  
+    # print(a.shape, "\n", b.shape)  
+    return np.linalg.lstsq(a, b, rcond=None)[0]
+
 # Finding source throught curve fit
 # Location from minimization
-def locationCF(measurement, detector, noise = [0, 0]):
+def locationCF(measurement, detector): #, noise = [0, 0]):
     HDs = measurement['m_dose']; dHDs = measurement['dm_dose']
     h = detector['h']; X = detector['X']; Y = detector['Y']; N_grid = int(np.sqrt(detector['m']))
 
-    # grid_x, grid_y
-    # hotspots
-    # grid_x_noise; grid_y_noise
-
     square_x = X/N_grid; square_y = Y/N_grid
-    grid_x = np.zeros((N_grid, N_grid)); grid_y = np.zeros((N_grid, N_grid))
-    grid_x_noise = noise[0]*np.ones((N_grid, N_grid)); grid_y_noise = noise[-1]*np.ones((N_grid, N_grid))
+
+    xs = np.linspace(-X/2 + square_x/2, X/2 - square_x/2, int(N_grid))
+    ys = np.linspace(-Y/2 + square_y/2, Y/2 - square_y/2, int(N_grid))
+    grid_x, grid_y = np.meshgrid(xs, np.flip(ys))
+    # grid_x_noise = noise[0]*np.ones((N_grid, N_grid)); grid_y_noise = noise[-1]*np.ones((N_grid, N_grid))
 
     i_max, j_max = unravel_index(HDs.argmax(), HDs.shape)
     x_c, y_c = grid_x[i_max, j_max], grid_y[i_max, j_max]
@@ -81,20 +88,9 @@ def locationCF(measurement, detector, noise = [0, 0]):
 
     x_0, x_1 = hotspot["xrange"]; y_0, y_1 = hotspot["yrange"]
 
-    if noise == [0, 0]:
-        XY = np.vstack((grid_x.ravel(), grid_y.ravel()))
-    else:
-        XY = np.vstack((grid_x_noise.ravel(), grid_y_noise.ravel()))
+    XY = np.vstack((grid_x.ravel(), grid_y.ravel()))
 
     u_est = random.uniform(x_0, x_1); v_est = random.uniform(y_0, y_1)
-
-    def parsEst2xN(HDs, grid_x, grid_y, h, u_est, v_est):
-        N = len(grid_x.flatten())
-        r = (grid_x.flatten() - np.ones((N))*u_est)**2 + (grid_y.flatten() - np.ones((N))*v_est)**2 + (np.ones((N))*h)**2
-        
-        a = np.rot90(np.array([1/r, np.ones(N)])); b = HDs.flatten()
-        
-        return np.linalg.lstsq(a, b, rcond=None)[0]
 
     alpha_est0, beta_est0 = parsEst2xN(HDs, grid_x, grid_y, h, u_est, v_est)
     
@@ -114,17 +110,15 @@ def locationCF(measurement, detector, noise = [0, 0]):
     popt, pcov = curve_fit(__dose, XY, HDs.ravel(), source0, sigma = dHDs.ravel(), absolute_sigma = True, method="lm", maxfev = 5000)
     perr = np.sqrt(np.diag(pcov))
 
-    # MyDict = {"XY": XY, "Ns": HDs, "source0": source0}
-
     return popt, perr, hotspot
 
 # Combination
-def field_combination(detector, measurement, noise=[0, 0]): #radiation
+def field_combination(measurement, detector): #radiation
     # h = detector['h']; K = detector['detector_constant']; dt = detector['dt']
     # F = radiation['dose_factor']
 
-    sourceCF, stDev = locationCF(measurement, detector, noise)[0], locationCF(measurement, detector, noise)[1]
-    hotspot = locationCF(measurement, detector, noise)[2]
+    sourceCF, stDev = locationCF(measurement, detector)[0], locationCF(measurement, detector)[1]
+    hotspot = locationCF(measurement, detector)[2]
 
     # alpha = sourceCF[2]; rel_alpha = 1/(sourceCF[2]/stDev[2])
     # A0 = (alpha)/(F*(1-K)*dt*h**2)
@@ -169,9 +163,47 @@ def visualize(data):
     plt.tight_layout()
     plt.show()
 
-# measurement = {'m_dose': np.array([[146.95,  19.61], [ 30.19,  12.82]]), 'dm_dose': np.array([[0.83, 0.3 ], [0.38, 0.24]])}
-# detector = {"h": 10, "X": 50, "Y": 50, "m": 4}
+measurement0 = {'m_dose': np.array([[113.092, 168.105, 179.2  , 127.064,  79.044,  50.134,  34.251,
+         24.115,  18.592,  13.405],
+       [ 90.629, 121.863, 129.108, 100.793,  67.333,  45.122,  31.85 ,
+         23.1  ,  17.584,  13.657],
+       [ 63.903,  76.783,  79.401,  68.054,  50.491,  36.967,  27.622,
+         21.119,  16.1  ,  13.09 ],
+       [ 43.281,  50.218,  50.106,  46.319,  37.072,  29.337,  23.177,
+         18.872,  15.001,  12.285],
+       [ 30.842,  33.929,  34.475,  32.753,  28.14 ,  23.443,  19.551,
+         15.946,  13.426,  10.997],
+       [ 23.534,  24.199,  24.871,  23.793,  20.489,  18.417,  16.191,
+         13.804,  11.599,  10.045],
+       [ 17.493,  17.997,  17.612,  17.794,  16.87 ,  14.861,  13.741,
+         11.683,  10.052,   8.722],
+       [ 13.993,  14.21 ,  14.623,  14.357,  13.251,  11.858,  11.473,
+         10.255,   9.387,   8.358],
+       [ 11.151,  11.473,  11.431,  11.172,  10.899,   9.926,   9.163,
+          8.589,   8.456,   7.329],
+       [  9.604,   9.562,   9.408,   9.478,   9.422,   9.009,   8.141,
+          7.588,   6.937,   6.391]]), 'dm_dose': np.array([[0.88974378, 1.08477417, 1.12      , 0.94310551, 0.74384676,
+        0.5924002 , 0.48964987, 0.41085886, 0.36075476, 0.30632499],
+       [0.79649419, 0.92360219, 0.95066082, 0.83997083, 0.68653551,
+        0.5620089 , 0.47217581, 0.40211939, 0.35083899, 0.30919088],
+       [0.6688206 , 0.73313096, 0.74552465, 0.69020142, 0.59450568,
+        0.50869342, 0.43972037, 0.38449057, 0.33570821, 0.30270448],
+       [0.55042438, 0.59289628, 0.59223475, 0.56941461, 0.50941535,
+        0.45316553, 0.40278903, 0.36346114, 0.32404784, 0.29324904],
+       [0.46464395, 0.4873428 , 0.49124841, 0.47882251, 0.44382429,
+        0.40509382, 0.36994189, 0.33409879, 0.30656484, 0.2774509 ],
+       [0.40587929, 0.41157381, 0.41724933, 0.4081066 , 0.37871229,
+        0.35905292, 0.33665561, 0.31085045, 0.28494385, 0.26516976],
+       [0.34992999, 0.35493521, 0.35111821, 0.35292775, 0.34364226,
+        0.32253217, 0.31014029, 0.28597378, 0.26526213, 0.24709108],
+       [0.31297124, 0.31538865, 0.31993906, 0.31701577, 0.30456034,
+        0.28810762, 0.28339195, 0.26792723, 0.25633767, 0.24188014],
+       [0.27938683, 0.28339195, 0.28287276, 0.27964978, 0.27621188,
+        0.26359439, 0.25326074, 0.24519992, 0.24329406, 0.22650166],
+       [0.25928363, 0.25871606, 0.25662424, 0.25757717, 0.25681511,
+        0.25112348, 0.2387195 , 0.23046909, 0.22036107, 0.21151123]])}
+detector = {"h": 10, "X": 50, "Y": 50, "m": 100}
 
-# data = locationCF(measurement, detector)
-# # print(type(data))
-# # visualize(data)
+# data = field_combination(measurement0, detector)
+
+# visualize(data)
